8f35843f1277de74f6a55cbaadc3e6493b7231bf
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-03 13:44:02 +0300

    Add project files.

---
+++ GoBing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using PlaywrightSharp;
+
+namespace GoBing
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(headless: false);
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("https://www.bing.com");
+            await page.ScreenshotAsync(path: "GoBind.png");
+        }
+    }
+}
